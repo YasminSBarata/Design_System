d672879d6d90004bc7115006d47e5b4de07f9020
Yasmin S. Barata <162582195+Yas-moon@users.noreply.github.com>
2025-05-13 15:28:15 -0300

    :books: docs: Ajustes no Main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,32 @@
-import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import { fileURLToPath } from 'url';
+import path from 'path';
+import type { StorybookConfig } from '@storybook/nextjs';
+
+// 👇 Converte import.meta.url para caminho de arquivo
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
+  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        //👇 Mockando o next/navigation com caminho absoluto compatível
+        ...(config.resolve.alias || {}),
+        'next/navigation': path.resolve(
+          dirname,
+          './mocks/next-navigation-mock.ts',
+        ),
+      };
+    }
+
+    return config;
+  },
 };
+
 export default config;
