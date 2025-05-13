776f69473eae7ceccca92dcd1a28175b9d085189
Yasmin S. Barata <162582195+Yas-moon@users.noreply.github.com>
2025-05-13 15:30:37 -0300

    :books: docs: Ajuste no container de renderização dos componentes

--- src/stories/atoms/link.stories.tsx
+++ src/stories/atoms/link.stories.tsx
@@ -7,7 +7,7 @@ const meta: Meta<typeof Link> = {
   tags: ['autodocs'],
   decorators: [
     (Story) => (
-      <div className='flex h-full w-full items-center justify-center bg-gray-600'>
+      <div className='flex h-full w-full items-center justify-center bg-amber-50'>
         <Story />
       </div>
     ),
